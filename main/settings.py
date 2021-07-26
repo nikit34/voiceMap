9590c7a3c4ccd430d776d1647298b872a795e0b4
nikit34 <permikov134@mail.ru>
2021-07-26 07:58:24 +0300

    beta

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django.contrib.gis',
+
     'markers',
 ]
 
@@ -77,7 +79,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
--- markers/views.py
+++ markers/views.py
@@ -1,5 +1,15 @@
+import json
+
 from django.views.generic.base import TemplateView
+from django.core.serializers import serialize
+
+from .models import Marker
 
 
 class MarkersMapView(TemplateView):
     template_name = "map.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["markers"] = json.loads(serialize("geojson", Marker.objects.all()))
+        return context
